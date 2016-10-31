22531dd5501614fc19cfc6a5d7758e1369a17ea3
troshinana <troshns@mail.ru>
2016-10-31 14:52:07 +0700

    Update task2_2.php

--- task2_2.php
+++ task2_2.php
@@ -8,28 +8,31 @@
 <form method="GET">
 Длина катета а = <input type="text" name="cathet1" value="<?php 
 		if (isset ($_GET['cathet1'])) {
-			echo $_GET['cathet1'];
+			echo htmlspecialchars($_GET['cathet1']);
 		}
 	        ?>">
 Длина катета b = <input type="text" name="cathet2" value="<?php 
 		if (isset ($_GET['cathet2'])) {
-			echo $_GET['cathet2'];
+			echo htmlspecialchars($_GET['cathet2']);
 		}
 			?>">
 <input type="submit" name="compute" value="Вычислить">
 </form>
 <?php
-function pythagoras($cathet1,$cathet2) {
-return(sqrt(pow(intval($cathet1),2)+pow(intval($cathet2),2)));
-}
-if (isset($_GET['cathet1']) && isset($_GET['cathet2'])) {
-	if ((!(is_numeric($_GET['cathet1'])))||(!(is_numeric($_GET['cathet2'])))) {
-	echo 'Значение катета может быть только числом!';
-	} elseif (($_GET['cathet1']<0)||($_GET['cathet2']<0)) {
-		echo "Значение катета должно быть не отрицательным!";} else {
-		echo number_format((pythagoras($_GET['cathet1'],$_GET['cathet2'])),2,',','');
+	function pythagoras($cathet1,$cathet2) {
+		return(sqrt(pow(intval($cathet1),2)+pow(intval($cathet2),2)));
+	}
+	if (isset($_GET['cathet1']) && isset($_GET['cathet2'])) {
+		$cathet1=$_GET['cathet1'];
+		$cathet2=$_GET['cathet2'];
+		if ((!(is_numeric($_GET['cathet1'])))||(!(is_numeric($_GET['cathet2'])))) {
+			echo 'Значение катета может быть только числом!';
+		} elseif (($_GET['cathet1']<0)||($_GET['cathet2']<0)) {
+			echo "Значение катета должно быть не отрицательным!";
+		} else {
+			echo number_format((pythagoras($_GET['cathet1'],$_GET['cathet2'])),2,',','');
 		}
-}
+	}
 
 
 ?>
